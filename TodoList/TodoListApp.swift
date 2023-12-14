b73212e80911664e5e290848d5f9020ee51e760b
Loren <liangliangpro@outlook.com>
2023-12-15 00:30:53 +0800

    feat: init

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by loren on 2023/12/15.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
